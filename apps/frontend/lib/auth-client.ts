54e26940ea4c3e9be7188a8715e840b24ba3ad0c
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2026-02-27 22:50:47 +0500

    Update auth-client.ts

--- apps/frontend/lib/auth-client.ts
+++ apps/frontend/lib/auth-client.ts
@@ -1,7 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
-export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || "https://todo-app-with-chatbot-js74.vercel.app",
-});
+// export const authClient = createAuthClient({
+//   baseURL: process.env.NEXT_PUBLIC_APP_URL || "https://todo-app-with-chatbot-js74.vercel.app",
+// });
 
-export const { signIn, signUp, signOut, useSession } = authClient;
+// export const { signIn, signUp, signOut, useSession } = authClient;
+export const authClient = createAuthClient({});
